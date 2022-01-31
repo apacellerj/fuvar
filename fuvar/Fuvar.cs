6455ba5c7663e031f18ab959a25f6f5b18fe0130
apacellerj <91457869+apacellerj@users.noreply.github.com>
2022-01-31 13:29:14 +0100

    5

---
+++ fuvar/Fuvar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuvar
+{
+    class Fuvar
+    {
+        public int TaxiID { get; set; }
+
+        public string Indulás { get; set; }
+
+        public int Időtartam { get; set; }
+
+        public double Távolság { get; set; }
+
+        public double Viteldíj { get; set; }
+
+        public double Borravaló { get; set; }
+
+        public string FizetésMód { get; set; }
+
+        public Fuvar(String sor)
+        {
+            string[] t = sor.Split(';');
+            TaxiID = int.Parse(t[0]);
+            Indulás = DateTime.Parse(t[1]);
+            Időtartam = int.Parse(t[2]);
+            Távolság = double.Parse(t[3]);
+            Viteldíj = double.Parse(t[4]);
+            Borravaló = double.Parse(t[5]);
+            FizetésMód = t[6];
+        }
+    }
+}
--- fuvar/Program.cs
+++ fuvar/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace fuvar
 {
@@ -10,6 +11,55 @@ namespace fuvar
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+
+            foreach (var sor in File.ReadAllLines("fuvar.csv").Skip(1))
+	        {
+                fuvarok.Add(new Fuvar(sor));
+	        }
+
+            Console.WriteLine($"3. feladat: {fuvarok.Count} fuvar");
+
+            double Bevétel = 0;
+            int db = 0;
+            foreach (var f in fuvarok)
+	        {
+                if (f.TaxiID == 6185)
+	            {
+                    Bevétel += f.Viteldíj + f.Borravaló;
+                    db++;
+	            }
+	        }
+            Console.WriteLine($"4. feladat: {db} fuvar alatt: {Bevétel}");
+            
+            /*
+            int bankkártyás = 0;
+            int készpénz = 0;
+
+            foreach (var f in fuvarok)
+	        {
+                if (f.FizetésMód == "bankkártya")
+	            {
+                    bankkártyás++;
+	            }
+                if (f.FizetésMód == "készpénz")
+	            {
+                    készpénz++;
+	            }
+	        }*/
+            
+            Dictionary<string, int> stat = new Dictionary<string, int>();
+            foreach (var f in fuvarok)
+	        {
+                if (stat.ContainsKey(f.FizetésMód))
+	            {
+                    stat[f.FizetésMód]++;
+	            }
+                else
+                {
+                    stat.Add(f.FizetésMód, 1);
+                }
+	        }
 
             Console.ReadKey();
         }
